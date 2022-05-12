3b4b10297bede5bad6f01339ea9f7018722a54e0
Joshua Bonner <joshua.bonner@outlook.com>
2022-05-12 09:18:21 -0400

    Completed search function

--- SpotifyAPI.py
+++ SpotifyAPI.py
@@ -1,5 +1,6 @@
 import base64
 import requests
+from urllib.parse import urlencode
 
 from common import APIQueryType
 
@@ -9,7 +10,7 @@ class SpotifyAPI(object):
     client_secret = None
     authorization_token = None
     token_url = 'https://accounts.spotify.com/api/token'
-    spotify_api_url = 'https://api.spotify.com/v1/search'
+    spotify_api_url = 'https://api.spotify.com/v1'
 
     def __init__(self, client_id: str, client_secret: str):
         self.client_id = client_id
@@ -17,9 +18,10 @@ class SpotifyAPI(object):
 
         self.authorize_client()
 
-    def authorize_client(self):
+    def authorize_client(self) -> bool:
         """Retrieves authorization token and authorizes client to use Spotify API
 
+        :return: boolean value that indicates whether or not the client is authorized
         """
 
         # set up credentials to be used in the retrieval of the Spotify API Token
@@ -29,7 +31,7 @@ class SpotifyAPI(object):
         token_headers = {'Authorization': f'Basic {client_credentials_b64.decode()}'}
         request = requests.post(url=self.token_url, data=token_data, headers=token_headers)
 
-        # if the request is invalid
+        # if the request is invalid handle failure
         if request.status_code not in range(200, 299):
             print(f'Request failed with error code: {request.status_code}')
             return False
@@ -40,5 +42,29 @@ class SpotifyAPI(object):
         print(f'Client authorized!')
         return True
 
-    def search(self, query_type: APIQueryType):
-        pass
+    def search(self, query: str, query_type: APIQueryType) -> dict:
+        """Searches for the query string by the APIQueryType using the Spotify API
+
+        :param query: The search query
+        :param query_type: Type of search {'artist', 'album', 'track'}
+        :return: search results in json format
+        """
+
+        # type check query type
+        if not isinstance(query_type, APIQueryType):
+            print('Invalid query type!')
+            return {}
+
+        # set up request
+        headers = {'Authorization': f'Bearer {self.authorization_token}'}
+        endpoint = self.spotify_api_url+'/search?'
+        data = urlencode({'q': query, 'type': query_type.value})
+        search_url = f'{endpoint}{data}'
+
+        # initiate request and return its output in a json dictionary
+        request = requests.get(search_url, headers=headers)
+        if request.status_code not in range(200, 299):
+            print(f'Request failed with error code: {request.status_code}')
+            return {}
+
+        return request.json()
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
+
 import SpotifyAPI
+from common import APIQueryType
 
 load_dotenv()
 CLIENT_ID = os.getenv('CLIENT_ID')
@@ -9,6 +11,7 @@ CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
 def main():
     client = SpotifyAPI.SpotifyAPI(CLIENT_ID, CLIENT_SECRET)
+    print(client.search('Time', APIQueryType.TRACK))
 
 
 if __name__ == '__main__':
