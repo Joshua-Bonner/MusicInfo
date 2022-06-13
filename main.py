baf96efb062b5a216456e5bab1c2b4187cc0305e
Joshua Bonner <joshua.bonner@outlook.com>
2022-06-13 16:47:23 -0400

    Committing progress

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
-import json
-import os
 import sys
-from dotenv import load_dotenv
+
+from PyQt5 import QtWidgets
 from GUI.MainGUIWindow import Ui_MainWindow
-from PyQt5 import QtCore, QtGui, QtWidgets
-
-from API import SpotifyAPI
-
-load_dotenv()
-CLIENT_ID = os.getenv('CLIENT_ID')
-CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
 
 def main():
-    client = SpotifyAPI.SpotifyAPI(CLIENT_ID, CLIENT_SECRET)
-    data = client.get_artist_info('i declare war')
-    print(json.dumps(data, indent=2))
+    # data = client.get_artist_info('i declare war')
+    # print(json.dumps(data, indent=2))
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
